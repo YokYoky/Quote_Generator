093fb86448032c10fae85bced80cd0973dc649d4
YokYoky <89176758+YokYoky@users.noreply.github.com>
2022-07-14 00:43:35 +0800

    Update quote.java

--- src/application/quote.java
+++ src/application/quote.java
@@ -5,16 +5,27 @@ import java.util.Scanner;
 public class quote {
 	static void quoteGen() {
 		Scanner in = new Scanner(System.in);
-		String quotes[] = {"hatdog - hatdog", "Skrrt Skrrt - lil pp", "Banana - Minion"};        
+		
+		// array to store quotes
+		String quotes[] = {"hatdog - hatdog", "Skrrt Skrrt - lil pp", "Banana - Minion"};     
+		
+		// instance of random class
 		Random r=new Random(); 
+		
+		// initialize the key variable
 		String key = "";
-	    int randomNumber=r.nextInt(quotes.length);
+		
+		// randomize the array
+	    int randomNumber = r.nextInt(quotes.length);
 	
 	
+	// loop the user input, if the the user input is empty it will loop
 	    while(key.isEmpty()) {
+		    
 	    	System.out.println("enter any key");
 	        key = in.nextLine();
 	        
+		// if the user enter key it will print the randomize array
 	        if(!key.isEmpty()) {
 	        	System.out.println(quotes[randomNumber]);
 	        }
