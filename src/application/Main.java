30230ae5214d105b68e1e58dc5d005255173cfe5
YokYoky <89176758+YokYoky@users.noreply.github.com>
2022-07-13 21:15:32 +0800

    Update Main.java

--- src/application/Main.java
+++ src/application/Main.java
@@ -4,24 +4,26 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main{ 
-	static Scanner in = new Scanner(System.in);
-	static void quoteGen() {
+	
+// you can put code in main class or just one class 
+// 	static Scanner in = new Scanner(System.in);
+// 	static void quoteGen() {
 		
-		String quotes[] = {"hatdog - hatdog", "Skrrt Skrrt - lil pp", "Banana - Minion"};        
-		Random r=new Random(); 
-		String key = "";
-	    int randomNumber=r.nextInt(quotes.length);
+// 		String quotes[] = {"hatdog - hatdog", "Skrrt Skrrt - lil pp", "Banana - Minion"};        
+// 		Random r=new Random(); 
+// 		String key = "";
+// 	    int randomNumber=r.nextInt(quotes.length);
 	
 	
-	    while(key.isEmpty()) {
-	    	System.out.println("enter any key");
-	        key = in.nextLine();
+// 	    while(key.isEmpty()) {
+// 	    	System.out.println("enter any key");
+// 	        key = in.nextLine();
 	        
-	        if(!key.isEmpty()) {
-	        	System.out.println(quotes[randomNumber]);
-	        }
-	    }
-	}
+// 	        if(!key.isEmpty()) {
+// 	        	System.out.println(quotes[randomNumber]);
+// 	        }
+// 	    }
+// 	}
     public static void main(String[] args) { 
     	quoteGen();
     }
